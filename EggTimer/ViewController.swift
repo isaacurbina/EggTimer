8c4063d21d1c55cf05ce0025477b06d5e533d830
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-14 14:56:07 -0600

    91. Calculating the Progress Percentage (#3)

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,22 +12,30 @@ class ViewController: UIViewController {
 	
 	@IBOutlet weak var topLabel: UILabel!
 	
+	@IBOutlet weak var progressBar: UIProgressView!
+	
 	// egg times in seconds
 	let eggTimes = [
 		"Soft" : 5 * 60,
 		"Medium": 8 * 60,
 		"Hard": 12 * 60
 	]
+	var cookingTime = 0
 	var secondsRemaining = 0
 	var timer = Timer()
     
 	@IBAction func hardnessSelected(_ sender: UIButton) {
 		timer.invalidate()
+		
+		progressBar.progress = 0.0
+		progressBar.alpha = 1.0
+		
 		topLabel.text = "How do you like your eggs?"
 		
 		let hardness = sender.currentTitle!
-		secondsRemaining = eggTimes[hardness] ?? 0
-	
+		cookingTime = eggTimes[hardness] ?? 0
+		secondsRemaining = cookingTime
+		
 		if (secondsRemaining > 0) {
 			timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 		}
@@ -35,10 +43,14 @@ class ViewController: UIViewController {
 	
 	@objc func updateTimer() {
 		if secondsRemaining > 0 {
-			print("\(secondsRemaining) seconds.")
 			secondsRemaining -= 1
+	
+			let progress = Float(cookingTime - secondsRemaining) / Float(cookingTime)
+			progressBar.progress = progress
 		} else {
 			topLabel.text = "DONE!"
+			progressBar.progress = 1.0
+			progressBar.alpha = 0.0
 			timer.invalidate()
 		}
 	}
