26ede8ddf7b26d7b0172695e879c2bab0aab6e02
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-14 14:36:38 -0600

    88. Egg Timer Challenge

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+	@IBOutlet weak var topLabel: UILabel!
+	
+	// egg times in seconds
 	let eggTimes = [
-		"Soft" : 5,
-		"Medium": 8,
-		"Hard": 12
+		"Soft" : 5 * 60,
+		"Medium": 8 * 60,
+		"Hard": 12 * 60
 	]
+	var secondsRemaining = 0
+	var timer = Timer()
     
 	@IBAction func hardnessSelected(_ sender: UIButton) {
+		timer.invalidate()
+		topLabel.text = "How do you like your eggs?"
+		
 		let hardness = sender.currentTitle!
-		print(eggTimes[hardness]!)
+		secondsRemaining = eggTimes[hardness] ?? 0
+	
+		if (secondsRemaining > 0) {
+			timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+		}
+	}
+	
+	@objc func updateTimer() {
+		if secondsRemaining > 0 {
+			print("\(secondsRemaining) seconds.")
+			secondsRemaining -= 1
+		} else {
+			topLabel.text = "DONE!"
+			timer.invalidate()
+		}
 	}
 }
 
